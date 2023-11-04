667af42910bf4ea9125392919a58413420d61053
VishnuPunith <93135223+VishnuPunith@users.noreply.github.com>
2023-11-04 14:54:58 +0530

    Update curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVER_KEY='AAAASMZ2ULc:APA91bEV_vpcLdgy_1G2T4e_2dCnHccKiVVNQzi-Kf9PU84aQ5RJpVaJZ0G84lVteOpLO2YVc9-WngpAz9z0fnk2ub_wNKqvye7T2S5cO6oZ2juc_KoHrji5Tpf-N_HbAw-15N1UH0s_'
-DEVICE_REG_TOKEN=''
+DEVICE_REG_TOKEN='cgUseOXZlDxDp1SLNcLnBL:APA91bGsqX8M8JLg5v7_uIBXE9rOdwfmCSohK8DlTm5RmKbnjnaqgaFBb1p3NIji2suoFltitlHjhsYtLvdkb5aYdITDso3NoUgNLazIPieDyLSgW9ep8E52k9MGqfuAhF_dET6iOxUX'
 CMD=$(cat <<END
 curl -X POST -H "Authorization: key=$SERVER_KEY" -H "Content-Type: application/json"
    -d '{
